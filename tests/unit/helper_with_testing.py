bc227e5fbb366843e2517ae1b1a18f32e924ad04
Suchy702 <wojtek.suchy6@wp.pl>
2022-06-28 19:22:38 +0200

    rename

--- tests/unit/helper_with_testing.py
+++ tests/unit/helper_with_testing.py
@@ -5,7 +5,7 @@ from src.worm import Worm
 from src.fish import Fish
 
 
-def get_obejct(kind='F', pos=Position(0, 0), speed=10, size=10, energy_val=15, pond_dim=(90, 160)):
+def get_object(kind='F', pos=Position(0, 0), speed=10, size=10, energy_val=15, pond_dim=(90, 160)):
     objects = {'F': Fish(speed, size, pos),
                'W': Worm(energy_val, pos, pond_dim),
                'A': Algae(energy_val, pos, pond_dim[0]),
