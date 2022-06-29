25f8943ae8fb0f48cdc19275d4f4929ad362209a
Łukasz Leszko <leszko.lucas@gmail.com>
2022-06-29 23:36:52 +0200

    Corrected test

--- tests/unit/pond_object_test.py
+++ tests/unit/pond_object_test.py
@@ -4,8 +4,8 @@ from src.pond_object import PondObject
 from src.position import Position
 
 
-def test_set_id_when_already_setted():
+def test_set_id_when_already_set():
     obj = PondObject('T', Position(0, 0))
-    obj.set_id(3)
+    obj.id = 3
     with pytest.raises(Exception):
-        obj.set_id(2)
+        obj.id = 2
