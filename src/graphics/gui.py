5b288d0c98ab36d78483bf5634127792085b6ce0
Łukasz Leszko <leszko.lucas@gmail.com>
2022-07-09 01:48:54 +0200

    Add centered zoom

--- src/graphics/gui.py
+++ src/graphics/gui.py
@@ -23,41 +23,33 @@ class GUI(EventHandler):
 
         self._screen: Surface = pygame.display.set_mode([self._settings.screen_width, self._settings.screen_height])
         self._cell_size: int = 20  # length of square cell in px
-        self.x_offset: int = 0
-        self.y_offset: int = 0
+        self._x_offset: int = 0
+        self._y_offset: int = 0
         self._center_view()
 
         self._event_handler = EventManager()
 
-    @property
-    def cell_size(self):
-        return self._cell_size
-
-    @cell_size.setter
-    def cell_size(self, val):
-        self._cell_size = min(max(10, val), 100)
-
-    def draw_frame(self):
+    def draw_frame(self) -> None:
         self._screen.fill((255, 255, 255))
         self.draw_squares(True)
         pygame.display.flip()
 
-    def draw_squares(self, show_grid=False):
+    def draw_squares(self, show_grid=False) -> None:
         visible_coordinates = self.get_visible_grid_coordinates()
         self.draw_full_squares(visible_coordinates, show_grid)
         self.draw_cut_squares(visible_coordinates, show_grid)
 
     # Get indices of cells that are fully visible
-    def get_visible_grid_coordinates(self):
-        x_min = int(ceil(-self.x_offset / self._cell_size))
-        x_max = (self._settings.screen_width - self._cell_size - self.x_offset) // self._cell_size
+    def get_visible_grid_coordinates(self) -> tuple[int, int, int, int]:
+        x_min = int(ceil(-self._x_offset / self._cell_size))
+        x_max = (self._settings.screen_width - self._cell_size - self._x_offset) // self._cell_size
 
-        y_min = int(ceil(-self.y_offset / self._cell_size))
-        y_max = (self._settings.screen_height - self._cell_size - self.y_offset) // self._cell_size
+        y_min = int(ceil(-self._y_offset / self._cell_size))
+        y_max = (self._settings.screen_height - self._cell_size - self._y_offset) // self._cell_size
 
         return x_min, x_max, y_min, y_max
 
-    def draw_full_squares(self, coor, show_grid=False):
+    def draw_full_squares(self, coor, show_grid=False) -> None:
         clipped_x = self.clip_x(coor[0], coor[1])
         clipped_y = self.clip_y(coor[2], coor[3])
 
@@ -65,15 +57,16 @@ class GUI(EventHandler):
             for j in range(clipped_y[0], clipped_y[1] + 1):
                 self._draw_images(i, j, show_grid)
 
-    def draw_cut_squares(self, coor, show_grid=False):
+    def draw_cut_squares(self, coor, show_grid=False) -> None:
         clipped_x = self.clip_x(coor[0], coor[1])
         clipped_y = self.clip_y(coor[2], coor[3])
 
-        is_left_column_cut = (self.x_offset % self._cell_size != 0)
+        is_left_column_cut = (self._x_offset % self._cell_size != 0)
         is_right_column_cut = (
-                self._settings.screen_width - ((clipped_x[1] + 1) * self._cell_size + self.x_offset) != 0)
-        is_top_row_cut = (self.y_offset % self._cell_size != 0)
-        is_bottom_row_cut = (self._settings.screen_height - ((clipped_y[1] + 1) * self._cell_size + self.y_offset) != 0)
+                self._settings.screen_width - ((clipped_x[1] + 1) * self._cell_size + self._x_offset) != 0)
+        is_top_row_cut = (self._y_offset % self._cell_size != 0)
+        is_bottom_row_cut = (
+                    self._settings.screen_height - ((clipped_y[1] + 1) * self._cell_size + self._y_offset) != 0)
 
         is_left_column_in_pond = (0 <= coor[0] - 1 < self._settings.pond_width)
         is_right_column_in_pond = (0 <= coor[1] + 1 < self._settings.pond_width)
@@ -117,10 +110,10 @@ class GUI(EventHandler):
                 is_bottom_row_cut or is_right_column_cut):
             self._draw_images(coor[1] + 1, coor[3] + 1, show_grid)
 
-    def clip_x(self, min_x, max_x):
+    def clip_x(self, min_x, max_x) -> tuple[int, int]:
         return max(min_x, 0), min(max_x, self._settings.pond_width - 1)
 
-    def clip_y(self, min_y, max_y):
+    def clip_y(self, min_y, max_y) -> tuple[int, int]:
         return max(min_y, 0), min(max_y, self._settings.pond_height - 1)
 
     def _get_images(self, x, y) -> list[Surface]:
@@ -135,7 +128,7 @@ class GUI(EventHandler):
 
     def _draw_images(self, x, y, show_grid=False) -> None:
         rect = pygame.Rect(
-            x * self._cell_size + self.x_offset, y * self._cell_size + self.y_offset,
+            x * self._cell_size + self._x_offset, y * self._cell_size + self._y_offset,
             self._cell_size, self._cell_size
         )
 
@@ -145,23 +138,40 @@ class GUI(EventHandler):
         for image in self._get_images(x, y):
             self._screen.blit(image, rect)
 
-    def _center_view(self):
-        self.x_offset = self._settings.screen_width // 2 - self._settings.pond_width * self._cell_size // 2
-        self.y_offset = self._settings.screen_height // 2 - self._settings.pond_height * self._cell_size // 2
+    def _center_view(self) -> None:
+        self._x_offset = self._settings.screen_width // 2 - self._settings.pond_width * self._cell_size // 2
+        self._y_offset = self._settings.screen_height // 2 - self._settings.pond_height * self._cell_size // 2
+
+    def _zoom(self, change: int) -> None:
+        old = self._cell_size
+        self._cell_size = min(max(10, self._cell_size + change), 100)
+
+        if self._cell_size == old:
+            return
+
+        # try to zoom at point in the middle
+        coor = self.get_visible_grid_coordinates()
+        middle = (coor[1] + coor[0]) / 2
+        left_half = (max(middle, 0) - max(coor[0], 0)) / 2
+        middle = (coor[3] + coor[2]) / 2
+        top_half = (max(middle, 0) - max(coor[2], 0)) / 2
+        print(left_half, top_half)
+        self._x_offset = int(self._x_offset - left_half * change)
+        self._y_offset = int(self._y_offset - top_half * change)
 
     def handle_events(self, events: list[Event]) -> None:
         for event in events:
             if event.event_type == EventType.KEY_PRESSED:
                 match event.args["key"]:
                     case "up":
-                        self.y_offset -= 50
+                        self._y_offset -= 50
                     case "down":
-                        self.y_offset += 50
+                        self._y_offset += 50
                     case "left":
-                        self.x_offset -= 50
+                        self._x_offset -= 50
                     case "right":
-                        self.x_offset += 50
+                        self._x_offset += 50
                     case "equals":
-                        self.cell_size += 5
+                        self._zoom(5)
                     case "minus":
-                        self.cell_size -= 5
+                        self._zoom(-5)
