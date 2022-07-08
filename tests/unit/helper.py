798b4c6695f9d0792daca2b8dafd3397fa3e00df
Suchy702 <wojtek.suchy6@wp.pl>
2022-07-08 14:35:36 +0200

    fixing tests

--- tests/unit/helper.py
+++ tests/unit/helper.py
@@ -11,6 +11,6 @@ def get_object(kind=ObjectKind.FISH, pos=Position(0, 0), speed=10, size=10, ener
         ObjectKind.FISH: Fish(speed, size, pos),
         ObjectKind.WORM: Worm(energy_val, pos, pond_dim),
         ObjectKind.ALGA: Alga(energy_val, pos, pond_dim[0]),
-        ObjectKind.ALGA_MAKER: AlgaMaker(pos),
+        ObjectKind.ALGA_MAKER: AlgaMaker(pos, pond_dim[0]),
     }
     return objects[kind]
