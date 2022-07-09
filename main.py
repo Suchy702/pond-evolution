e0ee02fa53dcf39c06163842c4bfe44cbc6b9702
Łukasz Leszko <leszko.lucas@gmail.com>
2022-07-09 13:18:26 +0200

    Add button manual

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from src.game import Game
 
 if __name__ == "__main__":
+    help_descr = """
+    Buttons:
+     arrows move camera
+     - or = changes zoom
+     , or . changes simulation speed
+     c centers camera
+    """
+    print(help_descr)
+
     game = Game()
     game.run()
