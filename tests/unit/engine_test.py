a7a4a4a143529713558a260d0a11a94eb5daa0d7
Łukasz Leszko <leszko.lucas@gmail.com>
2022-07-11 13:50:47 +0200

    Fix test

--- tests/unit/engine_test.py
+++ tests/unit/engine_test.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
+
 import pytest
 
 from src.engine import Engine
+from src.events.event_emitter import EventEmitter
 from src.simulation_settings import SimulationSettings
 
 
@@ -9,6 +12,7 @@ def sample_engine():
     return Engine(SimulationSettings(10, 10))
 
 
+@patch.object(EventEmitter, 'emit_event')
 def test_cycle_does_work_properly(sample_engine):
     sample_engine._interactor.preparations()
     for _ in range(5):
