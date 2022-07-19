8dc03868b3df16e7b2e5aad9b2d7dd2e39b1194e
Łukasz Leszko <leszko.lucas@gmail.com>
2022-07-19 03:19:45 +0200

    Add test

---
+++ tests/integration/game_test.py
@@ -0,0 +1,34 @@
+import os
+from unittest.mock import patch, Mock
+
+import pygame
+import pytest
+
+from src.game import Game
+
+IMG_DIR_PATH = os.path.join('..', 'resources', 'object_images')
+
+
+class SimulationSettingsMock:
+    screen_pond_height = 640
+    screen_pond_width = 1080
+    pond_height = 32
+    pond_width = 54
+    screen_height = 720
+    screen_width = 1080
+    get_user_settings = Mock()
+
+
+@patch('pygame.event.get')
+@patch('src.game.SimulationSettings', new=SimulationSettingsMock)
+@patch('src.graphics.image_handler.image_handler.IMG_DIR_PATH', IMG_DIR_PATH)
+@pytest.mark.timeout(30)
+def test_game(event_get_mock):
+    events: list[any] = [[Mock()]] * 100 + [[pygame.event.Event(pygame.locals.QUIT)]]
+    event_get_mock.side_effect = events
+
+    game = Game()
+    game.run()
+
+    assert SimulationSettingsMock.get_user_settings.call_count == 1
+    assert event_get_mock.call_count == 101
