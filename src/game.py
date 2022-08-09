b5c0e8cb88d0d5b00bbbb0efa74db5c212b2b9af
Suchy702 <wojtek.suchy6@wp.pl>
2022-08-09 15:03:27 +0200

    game refactor

--- src/game.py
+++ src/game.py
@@ -45,25 +45,25 @@ class Game:
     def _user_close_program(self):
         return self._settings.screen_height is None
 
-    def _end_game_actions(self):
+    def _end_game_actions(self) -> None:
         self._gui.hide_screen()
         self._statistics.show_statistics()
 
-    def _skipped_game(self):
+    def _skipped_game(self) -> None:
         self._event_emitter.clear_gui_events()
         self._event_emitter.handle_events()
         self._engine.cycle()
         self._statistics.make_snapshot()
         self.skip -= 1
 
-    def _normal_game(self):
+    def _normal_game(self) -> None:
         if self._gui.is_animation_finished():
             self._engine.cycle()
             self._statistics.make_snapshot()
 
         self._event_emitter.handle_events()
 
-    def _game_type_decision(self):
+    def _game_type_decision(self) -> None:
         if self.skip:
             self._skipped_game()
         else:
