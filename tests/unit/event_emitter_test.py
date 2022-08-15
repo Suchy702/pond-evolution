0e8d4de17640915ac602ce23ed1f5cf42f84245c
Łukasz Leszko <leszko.lucas@gmail.com>
2022-08-15 22:33:59 +0200

    Fix tests

--- tests/unit/event_emitter_test.py
+++ tests/unit/event_emitter_test.py
@@ -13,15 +13,15 @@ def event_emitter():
 
 
 def test_emit_event(event_emitter):
-    event_emitter.graphic_event_manager = Mock()
+    event_emitter._graphic_event_manager = Mock()
     graphic_event = GraphicEvent(GraphicEventType.ANIM_MOVE)
     event_emitter.emit_event(graphic_event)
-    event_emitter.graphic_event_manager.add_event.assert_called_once()
+    event_emitter._graphic_event_manager.add_event.assert_called_once()
 
 
 def test_emit_events(event_emitter):
-    event_emitter.graphic_event_manager = Mock()
+    event_emitter._graphic_event_manager = Mock()
     graphic_event = GraphicEvent(GraphicEventType.ANIM_MOVE)
     events = [graphic_event.copy() for _ in range(10)]
     event_emitter.emit_events(events)
-    assert event_emitter.graphic_event_manager.add_event.call_count == 10
+    assert event_emitter._graphic_event_manager.add_event.call_count == 10
