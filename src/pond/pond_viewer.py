715777f0b4970e4a9ec59ca5b5cfad173679ee6f
Suchy702 <wojtek.suchy6@wp.pl>
2022-08-18 09:34:31 +0200

    pond_viewer refactor

--- src/pond/pond_viewer.py
+++ src/pond/pond_viewer.py
@@ -33,7 +33,7 @@ class PondViewer:
             self, pos: Position, eyesight: int, obj_type: list[ObjectKind], negate: bool = False
     ) -> Generator[list[PondObject], None, None]:
         """
-        If negate is false return objects of one of the types specified in `obj_type`. Otherwise return objects whose
+        If negate is false return objects of one of the types specified in `obj_type`. Otherwise, return objects whose
         type is not in `obj_type`.
         """
         yield from self._get_visible_objects(pos, eyesight, lambda obj: obj.kind in obj_type, negate)
@@ -42,11 +42,11 @@ class PondViewer:
             self, pos: Position, eyesight: int, traits: list[FishTrait], negate: bool = False
     ) -> Generator[list[Fish], None, None]:
         """
-        If negate is false return objects whose traits are subset of `traits`. Otherwise return objects whose traits
+        If negate is false return objects whose traits are subset of `traits`. Otherwise, return objects whose traits
         are disjoint with `traits`.
         """
 
-        def filter(obj: PondObject) -> bool:
+        def filter_(obj: PondObject) -> bool:
             fish = cast("Fish", obj)
             return all(trait in traits for trait in fish.traits)
 
@@ -54,7 +54,7 @@ class PondViewer:
             new_list = []
             for fish in fish_layer:
                 fish = cast("Fish", fish)
-                if self._check_condition(fish, filter, negate):
+                if self._check_condition(fish, filter_, negate):
                     new_list.append(fish)
             if new_list:
                 yield new_list
@@ -87,6 +87,9 @@ class PondViewer:
 
         return objects
 
+    def _is_in_pond(self, pos: Position) -> bool:
+        return 0 <= pos.x < self.pond_width and 0 <= pos.y < self.pond_height
+
     def _get_corner_objects(
             self, offset: list[tuple[int, int]], radius: int, pos: Position, obj_filter: Callable[[PondObject], bool],
             negate: bool = False
@@ -95,10 +98,28 @@ class PondViewer:
 
         for (off_x, off_y) in offset:
             point = Position(pos.y + off_y * radius, pos.x + off_x * radius)
-            if 0 <= point.x < self.pond_width and 0 <= point.y < self.pond_height:
+            if self._is_in_pond(point):
                 objects.extend(self._get_spot_objects(point, obj_filter, negate))
 
         return objects
+
+    def _get_edge_object(
+            self, objects: list[PondObject], dummy_p1: tuple[int, int], dummy_p2: tuple[int, int], pos: Position,
+            radius: int, obj_filter: Callable[[PondObject], bool], negate: bool
+    ) -> None:
+        x_change = dummy_p2[0] - dummy_p1[0]
+        y_change = dummy_p2[1] - dummy_p1[1]
+        p1 = Position(pos.y + dummy_p1[1] * radius + y_change, pos.x + dummy_p1[0] * radius + x_change)
+        p2 = Position(pos.y + dummy_p2[1] * radius - y_change, pos.x + dummy_p2[0] * radius - x_change)
+
+        (first_idx, last_idx) = self._get_intersection_indices(p1, p2)
+
+        if first_idx is None:
+            return
+
+        for i in range(first_idx, last_idx + 1):
+            point = Position(p1.y + y_change * i, p1.x + x_change * i)
+            objects.extend(self._get_spot_objects(point, obj_filter, negate))
 
     def _get_edge_objects(
             self, offset: list[tuple[int, int]], radius: int, pos: Position, obj_filter: Callable[[PondObject], bool],
@@ -107,35 +128,41 @@ class PondViewer:
         objects = []
 
         for dummy_p1, dummy_p2 in itertools.pairwise(offset + [(0, 1)]):
-            x_change = dummy_p2[0] - dummy_p1[0]
-            y_change = dummy_p2[1] - dummy_p1[1]
-            p1 = Position(pos.y + dummy_p1[1] * radius + y_change, pos.x + dummy_p1[0] * radius + x_change)
-            p2 = Position(pos.y + dummy_p2[1] * radius - y_change, pos.x + dummy_p2[0] * radius - x_change)
-
-            (first_idx, last_idx) = self._get_intersection_indices(p1, p2)
-
-            if first_idx is None:
-                continue
-
-            for i in range(first_idx, last_idx + 1):
-                objects.extend(
-                    self._get_spot_objects(
-                        Position(p1.y + y_change * i, p1.x + x_change * i),
-                        obj_filter,
-                        negate
-                    )
-                )
+            self._get_edge_object(objects, dummy_p1, dummy_p2, pos, radius, obj_filter, negate)
 
         return objects
 
-    def _get_intersection_indices(self, p1, p2) -> tuple[Optional[int], Optional[int]]:
-        x_swapped, y_swapped = False, False
+    @staticmethod
+    def _swapping_x(p1: Position, p2: Position) -> bool:
         if p1.x > p2.x:
             p1.x, p2.x = p2.x, p1.x
-            x_swapped = True
+            return True
+        return False
+
+    @staticmethod
+    def _swapping_y(p1: Position, p2: Position) -> bool:
         if p1.y > p2.y:
             p1.y, p2.y = p2.y, p1.y
-            y_swapped = True
+            return True
+        return False
+
+    @staticmethod
+    def _restore_coors(p1: Position, p2: Position, x_swapped: bool, y_swapped: bool) -> None:
+        if x_swapped:
+            p1.x, p2.x = p2.x, p1.x
+        if y_swapped:
+            p1.y, p2.y = p2.y, p1.y
+
+    @staticmethod
+    def _hold_indices_order(x_indices: list[int], y_indices: list[int]):
+        if x_indices[0] > x_indices[1]:
+            x_indices[0], x_indices[1] = x_indices[1], x_indices[0]
+        if y_indices[0] > y_indices[1]:
+            y_indices[0], y_indices[1] = y_indices[1], y_indices[0]
+
+    def _calc_indices(self, p1: Position, p2: Position) -> tuple[Optional[list[int]], Optional[list[int]]]:
+        x_swapped = self._swapping_x(p1, p2)
+        y_swapped = self._swapping_y(p1, p2)
 
         x_range = self._get_intersection(p1.x, p2.x, 0, self.pond_width - 1)
         y_range = self._get_intersection(p1.y, p2.y, 0, self.pond_height - 1)
@@ -143,19 +170,19 @@ class PondViewer:
         if x_range[0] > x_range[1] or y_range[0] > y_range[1]:
             return None, None
 
-        if x_swapped:
-            p1.x, p2.x = p2.x, p1.x
-        if y_swapped:
-            p1.y, p2.y = p2.y, p1.y
+        self._restore_coors(p1, p2, x_swapped, y_swapped)
 
         x_indices = [abs(x_range[0] - p1.x), abs(x_range[1] - p1.x)]
         y_indices = [abs(y_range[0] - p1.y), abs(y_range[1] - p1.y)]
 
-        if x_indices[0] > x_indices[1]:
-            x_indices[0], x_indices[1] = x_indices[1], x_indices[0]
+        return x_indices, y_indices
 
-        if y_indices[0] > y_indices[1]:
-            y_indices[0], y_indices[1] = y_indices[1], y_indices[0]
+    def _get_intersection_indices(self, p1: Position, p2: Position) -> tuple[Optional[int], Optional[int]]:
+        x_indices, y_indices = self._calc_indices(p1, p2)
+        if x_indices is None:
+            return None, None
+
+        self._hold_indices_order(x_indices, y_indices)
 
         indices = self._get_intersection(x_indices[0], x_indices[1], y_indices[0], y_indices[1])
         if indices[0] > indices[1]:
