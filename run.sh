b623dad0eef0c30b89a81f08ca201062733d7526
Łukasz Leszko <leszko.lucas@gmail.com>
2022-08-19 02:03:30 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ ! -e installed ]]; then
+    echo "Installing required dependencies..."
+    pip install -r "./requirements.txt"
+    touch installed
+fi
+
 python main.py &> /dev/null
